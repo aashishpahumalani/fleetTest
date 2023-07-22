c5d1004f96078c8c649e7aff487d33c98eb5ce33
aashishpahumalani <apahumalani@gmail.com>
2023-07-22 16:56:36 +0530

    task is completed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const {Octokit} = require("octokit");
-const fetch = require('node-fetch');
+const { Octokit } = require("octokit");
+const fetch = require("node-fetch");
 
 //create express app
 const app = express();
@@ -9,51 +9,78 @@ const app = express();
 const port = 3000;
 
 const octokit = new Octokit({
-  auth: 'TOKEN',
-  request: {fetch}
-})
-
-//to get the commit detail using id
-app.get("/repositories/:owner/:repository/commits/:oid", async (request, response) => {
-
-  const {owner, repository, oid} = request.params
-  
-  const res = await octokit.request(`GET /repos/{owner}/{repo}/commits/{ref}`, {
-    owner,
-    repo: repository,
-    ref: oid,
-    headers: {
-      'X-GitHub-Api-Version': '2022-11-28'
-    }
-  })
-
-  const data = res.data;
-  const responseData = {
-    oid:data.sha,
-    message: data.commit.message,
-    author: data.commit.author,
-    committer: data.commit.committer,
-    parents: data.parents.map((parent)=>({oid: parent.sha}))}
-  response.json([responseData]);
+  auth: "Token",
+  request: { fetch },
 });
 
+//to get the commit detail using id
+app.get(
+  "/repositories/:owner/:repository/commits/:oid",
+  async (request, response) => {
+    const { owner, repository, oid } = request.params;
+    if (!owner || !repository || !oid) {
+      return response.json({ message: "Please Add All Required Parameters" });
+    }
+
+    try {
+      const res = await octokit.request(
+        `GET /repos/{owner}/{repo}/commits/{ref}`,
+        {
+          owner,
+          repo: repository,
+          ref: oid,
+          headers: {
+            "X-GitHub-Api-Version": "2022-11-28",
+          },
+        }
+      );
+
+      const data = res.data;
+      const responseData = {
+        oid: data.sha,
+        message: data.commit.message,
+        author: data.commit.author,
+        committer: data.commit.committer,
+        parents: data.parents.map((parent) => ({ oid: parent.sha })),
+      };
+      return response.json([responseData]);
+    } catch (error) {
+      return response.status(error.status ?? 500).json({
+        message: error?.response?.data?.message ?? "Internal Server Error",
+      });
+    }
+  }
+);
+
 //to get the commit difference
-app.get("/repositories/:owner/:repository/commits/:oid/diff", async (request, response) => {
+app.get(
+  "/repositories/:owner/:repository/commits/:oid/diff",
+  async (request, response) => {
+    const { owner, repository, oid } = request.params;
+    if (!owner || !repository || !oid) {
+      return response.json({ message: "Please Add All Required Parameters" });
+    }
+    try {
+      const res = await octokit.request(
+        `GET /repos/{owner}/{repo}/compare/{basehead}`,
+        {
+          owner,
+          repo: repository,
+          basehead: oid,
+          headers: {
+            "X-GitHub-Api-Version": "2022-11-28",
+          },
+        }
+      );
 
-    const {owner, repository, oid} = request.params
-    
-    const res = await octokit.request(`GET /repos/{owner}/{repo}/compare/{basehead}`, {
-      owner,
-      repo: repository,
-      basehead: oid,
-      headers: {
-        'X-GitHub-Api-Version': '2022-11-28'
-      }
-    })
-  
-    response.json({data:res.data});
-  });
-
+      return response.json({ data: res.data });
+    } catch (error) {
+      return response.status(error.status ?? 500).json({
+        message: error?.response?.data?.message ?? "Internal Server Error",
+      });
+    }
+  }
+);
 
 //start server and listen for the request
 app.listen(port, () =>
